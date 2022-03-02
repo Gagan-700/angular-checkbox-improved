55f2b9a436b19944da033482a30557873e6c2acb
Gagan-700 <gagan.jaiswal2@gmail.com>
2022-03-02 16:41:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-coosa5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-coosa5)
---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  isChecked;
+  toggle = false;
+  data = [];
+  accessCheck: boolean[] = [];
+  tableData = [
+    {
+      company: 'xyz',
+      access: false,
+      primary: true,
+      cin: '456378',
+      status: 'Open',
+    },
+    {
+      company: 'abc',
+      access: true,
+      primary: false,
+      cin: '87654',
+      status: 'Open',
+    },
+    {
+      company: 'wdc',
+      access: false,
+      primary: true,
+      cin: '400087',
+      status: 'Open',
+    },
+  ];
+  updateAccessCheck() {
+    this.tableData.map((x) => {
+      this.accessCheck.push(x.access);
+    });
+  }
+  update() {
+    this.toggle = !this.toggle;
+    // alert(this.isChecked)
+  }
+  submit() {
+    console.log(this.data, 'data');
+  }
+  checkbox(value, id) {
+    console.log(id);
+    this.data.push(this.tableData[id]);
+    console.log(this.data);
+  }
+}
